1fa5670ed193a91346268ce4140de89e35f9c775
srikar <srikarcracker1@gmail.com>
2023-03-15 18:16:57 +0530

    Initial Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { DatepickerComponent } from './datepicker/datepicker.component';
+import { DealsComponent } from './deals/deals.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+
+const routes: Routes = [
+  {
+    path:'about',
+    component:AboutComponent
+  },
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'contact',
+    component:ContactComponent
+  },
+  {
+    path:'datepicker',
+    component:DatepickerComponent
+  },
+  {
+    path:'profile',
+    component:ProfileComponent
+  },
+  {
+    path:'deals',
+    component:DealsComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
